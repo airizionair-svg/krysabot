83b265252f923bd17e14c6621d2eb5c6759dff6d
airizionair-svg <airizionair@gmail.com>
2026-06-16 22:14:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Запуск PetriDish Monitor Bot
-
-# Установка зависимостей
 pip install -r requirements.txt -q
-
-# Установка браузеров для Playwright
-playwright install chromium
-
-# Запуск бота
 python bot.py
